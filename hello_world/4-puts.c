8246bac223388f8b4116296fead76ae7ea114688
AngeiraT <4985@holbertonschool.com>
2022-06-08 00:16:21 +0000

    prints message

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 /**
  * Start with main
- * Should end with the value 0
+ * Return Should end with the value 0
  * /
-int main 
+int main
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
