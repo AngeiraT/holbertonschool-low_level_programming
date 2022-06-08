ec7432a4d7116aba0625c4ca1db6f402a1905d4e
AngeiraT <4985@holbertonschool.com>
2022-06-08 17:24:24 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Starting the execution
+ *
+ * Return: Always 0 (program will execute successfully)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
