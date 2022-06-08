f497a45d4893665dd2fe5496d9b3f4ea9e492430
AngeiraT <4985@holbertonschool.com>
2022-06-08 18:44:49 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Starting the execution
+ *
+ * Return: Always 0 (program will execute successfully)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+
+	printf("%d", i);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
