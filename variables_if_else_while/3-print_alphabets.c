8f83dd080807180633bdd2c8358b9aa9ce37fe1e
AngeiraT <4985@holbertonschool.com>
2022-06-08 17:47:16 +0000

    alphaBET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,13 @@
  */
 int main(void)
 {
-	char i;
+	char i, b;
 
 	for (i = 'A'; i <= 'Z'; i++)
 		putchar(i);
 
-	putchar('\n');
-
-	for (i = 'A'; i <= 'Z'; i++)
-		putchar(i);
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
 	putchar('\n');
 
 	return (0);
