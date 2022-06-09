736644d33d84ea1081945d980dbb02a4170e2fee
AngeiraT <4985@holbertonschool.com>
2022-06-09 17:26:30 +0000

    10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ *Return: Void
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+	putchar(l + '0');
+		}
+	putchar('\n');
+}
+
