39a5e2de1006adf4236114c266661eef78f2e5c9
AngeiraT <4985@holbertonschool.com>
2022-06-10 02:16:50 +0000

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,17 +8,11 @@
 
 int print_last_digit(int n)
 {
-	int last;
-
-	if (n < 0)
-	{
-		n = -n;
-	}
-	last = n % 10;
+	int last = n % 10;
 
 	if (last < 0)
 	{
-		last = -last;
+		last *= -1;
 
 		_putchar(last + '0');
 	}
