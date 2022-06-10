1a631b2e3336f952e6b14c096b89ee23092c7f3c
AngeiraT <4985@holbertonschool.com>
2022-06-10 00:56:57 +0000

    absolute

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - Entry point
+ *@n: absolute number of an integer
+ *Return: Void
+ */
+
+int _abs(int n)
+{
+	if(n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
