6fa1621baa8841459ee2227f07a97cf9e5e6f476
AngeiraT <4985@holbertonschool.com>
2022-06-13 14:03:11 +0000

    uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isupper - Entry point
+ *@c: uppercase
+ *Return: Sucess 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		{
+		return (1);
+		}
+	else
+		{
+		return (0);
+		}
+}
