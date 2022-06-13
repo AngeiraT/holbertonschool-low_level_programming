3966b7a7463a73d02949a7e96a69270e3903b6e6
AngeiraT <4985@holbertonschool.com>
2022-06-13 14:33:32 +0000

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_numbers - Entry point
+ *@n: numbers from 0-9
+ *Return: Void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+
+		_putchar(n);
+
+	}
+	_putchar('\n');
+
+}
