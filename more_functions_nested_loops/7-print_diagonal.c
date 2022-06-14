c5b9d945d58147387c75a9c53b8ee02d1032c4b9
AngeiraT <4985@holbertonschool.com>
2022-06-14 14:41:28 +0000

    diagonal lines

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,20 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n > 0)
-
-		for (i = 1; i <= n; i++)
 	{
-			for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
 				_putchar (' ');
-	}
-	_putchar(47);
+			}
+
+	_putchar(92);
 	_putchar('\n');
+		}
+	}
+	else
+
+		_putchar('\n');
+
 }
