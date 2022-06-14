57989f56ce907cd6c4ae86f147c87480f469ac78
AngeiraT <4985@holbertonschool.com>
2022-06-14 17:33:17 +0000

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Starting the execution
+ *
+ * Return: Always 0 (program will execute successfully)
+ */
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+
+			printf("FizzBuzz");
+
+		else if (num % 3 == 0)
+
+			printf("Fizz");
+
+		else if (num % 5 == 0)
+
+			printf("Buzz");
+
+		else
+
+			printf("%d ", num);
+
+
+		if (num != 100)
+
+		printf(" ");
+		num++;
+
+	}
+	printf("\n");
+	return (0);
+}
