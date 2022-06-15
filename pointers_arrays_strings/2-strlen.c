93c50fce3cdf45c4260e99397093c872613c5c21
AngeiraT <4985@holbertonschool.com>
2022-06-15 00:34:31 +0000

    srtlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - Entry point
+ *@s: lenght of char
+ *Return: int - lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int lenght;
+
+	for (lenght = 0; s[lenght] != '\0'; lenght++)
+	{
+		printf("%d", lenght);
+	}
+	return (lenght);	
+}
