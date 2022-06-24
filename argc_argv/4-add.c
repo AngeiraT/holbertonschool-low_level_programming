9b29babc0e14fb133c5bc98d918d7be6306f2fe5
AngeiraT <4985@holbertonschool.com>
2022-06-24 14:30:58 +0000

    adding positive

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,3 +9,17 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+	i = 0;
+
+	if (argc == 3)
+	{
+	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		i = 1;
+	}
+	return (i);
+}
