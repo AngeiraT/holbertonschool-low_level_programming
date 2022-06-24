8b51ddfab8288da1e292786b38012691086e3c5d
AngeiraT <4985@holbertonschool.com>
2022-06-24 13:22:13 +0000

    add positive

---
+++ argc_argv/4-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *@argc: number of arguments
+ *@argv: character of strings
+ *Return: Sucess 0
+ */
+
+int main(int argc, char *argv[])
+{
