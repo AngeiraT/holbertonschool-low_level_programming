189285ad216816013af2e4821854469b80386897
AngeiraT <4985@holbertonschool.com>
2022-06-25 23:28:13 +0000

    joint strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Entry point
+ * @s1: string one
+ * @s2: string two
+ * Return: Nothing.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *new;
+	unsigned int i;
+	unsigned int j;
+	int total = 0;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	total += strlen(s1) + strlen(s2);
+	new = malloc((total * sizeof(char)) + 1);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		new[i] = s1[i];
+	}
+	for (j = 0; s2[j]; j++, i++)
+	{
+		new[i] = s2[j];
+	}
+	new[i] = '\0';
+	return (new);
+}
