e373929935edb134cc763f81704ec45dbccfb956
AngeiraT <4985@holbertonschool.com>
2022-06-25 22:52:36 +0000

    copy string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Entry point
+ * @str: copy string
+ * Return: Nothing.
+ */
+
+char *_strdup(char *str)
+{
+	int lenght = strlen(str);
+	int i;
+	char *copy;
+
+	copy = malloc(sizeof(char) * lenght);
+
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < lenght; i++)
+	{
+		copy[i] = str[i];
+	}
+	return (copy);
+}
