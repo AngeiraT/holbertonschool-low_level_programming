54479f9bdafb02f20f5f3ce1da5730e13edf3592
AngeiraT <4985@holbertonschool.com>
2022-07-16 15:02:38 +0000

    free head command

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,11 +7,13 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (temp != NULL)
+	while (head != NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	
 	}
 }
 
