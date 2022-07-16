cfca52127455ba3b9c5d3152c259c1a4a5342236
AngeiraT <4985@holbertonschool.com>
2022-07-16 02:44:37 +0000

    change to while outside if

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,20 +9,18 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *ptr;
-	size_t count = 0;
+	unsigned int count = 0;
 
 	ptr = h;
 
-	if (h == NULL)
+	while (ptr != NULL)
 	{
-		printf("[0] (nil)\n");
-
-		while (ptr != NULL)
-		{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
 			count++;
-			ptr = ptr->next;
-		}
 	}
-	printf("[%i] %s\n", ptr->len, ptr->str);
 	return (count);
 }
