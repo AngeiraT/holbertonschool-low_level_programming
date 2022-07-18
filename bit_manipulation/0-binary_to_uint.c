b62daeee7d4c625b532c3fc1093b8b4b72fe1d8e
AngeiraT <4985@holbertonschool.com>
2022-07-18 17:25:30 +0000

    add if is not 0 and 1

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,14 +13,16 @@ unsigned int binary_to_uint(const char *b)
 	int decimalval = 1;
 	int idx;
 
-	if (b == NULL)
-		return (0);
-
 	for (idx = (slen - 1); idx >= 0; idx--)
 	{
 		if (b[idx] == '1')
 		total += decimalval;
 		decimalval *= 2;
+
+		if (b[idx] != '0' && b[idx] != '1')
+		{
+			return (0);
+		}
 	}
 	return (total);
 }
