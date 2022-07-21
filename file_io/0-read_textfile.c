17e44ab025695301ad7e94e96806bf0b494b4127
AngeiraT <4985@holbertonschool.com>
2022-07-21 00:48:05 +0000

    created the file rquiescat

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it
+ * @letters: number of letters it should read and
+ * and print
+ * @filename: pointer to file
+ *
+ * Return: numbers of letters it could read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	int count;
+
+	/**
+	* assign it a memory space check malloc
+	*/
+	buffer = malloc(sizeof(size_t) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	count = read(fd, buffer, letters);
+	if (count < 0)
+		return (0);
+
+	count = write(1, buffer, count);
+	if (count < 0)
+		return (0);
+
+	close(fd);
+	return (count);
+}
+
