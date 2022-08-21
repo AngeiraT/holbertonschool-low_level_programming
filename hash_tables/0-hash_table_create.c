6e55ff7c1f558f66c956c4839d18036dd5378f35
AngeiraT <thaispur04@icloud.com>
2022-08-21 17:18:58 -0400

    fixed nil

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,12 +13,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL || size == 0)
 		return (NULL);
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
 
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-		free(ht);
-	return (NULL);
+		return (NULL);
+	ht->size = size;
 
 	for (idx = 0; idx < size; idx++) /*Initializes array slots to NULL*/
 		ht->array[idx] = NULL;
